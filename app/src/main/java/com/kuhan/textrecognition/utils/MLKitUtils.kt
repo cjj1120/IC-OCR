15debe0ef3410e3a9b2ed243c8edc3fdc15774b4
Your Name <cjj8168@gmail.com>
2021-12-29 13:56:03 +0800

    update

--- app/src/main/java/com/kuhan/textrecognition/utils/MLKitUtils.kt
+++ app/src/main/java/com/kuhan/textrecognition/utils/MLKitUtils.kt
@@ -37,19 +37,43 @@ class MLKitUtils {
     fun getTextFile(text: MLKitText): File? {
         return try {
             val parsedText = MLKitUtils().getTextBlock(text)
-
             //MY TODO: Extract relevant info, and print out !!
 
-            Log.e("IC-TAG", parsedText.textBlocks.get(2).string)  // This will print kad pengenalan !! Found it !!
-            Log.e("NAME-TAG", parsedText.textBlocks.get(4).string)
 
 //            Log.e("NAME-TAG", parsedText.textBlocks.get(4).lines[1].string)
 //            Log.e("TAG", parsedText.textBlocks.toString())
 //            Log.e("TAG", parsedText.textBlocks.javaClass.name) //THIS IS ARRAY LIST
 //            Log.e("TAG", parsedText.text.javaClass.name) //THIS IS STRING
+            var extractedText = ""
+            val symbols = "0123456789"
+            var icBoundingBoxNum = 0;
 
-            Log.e("IC", parsedText.text.split(" ").toString())
-            Log.e("TAG5", parsedText.text.split(" ")[4])
+            Log.e("test",  parsedText.textBlocks.size.toString())
+            for (i in 1..6) {
+                extractedText = parsedText.textBlocks.get(i).string
+//                Log.e("test", i.toString())
+//                Log.e("test", extractedText)
+                val x = extractedText.contains("[0-9]".toRegex());
+                if ((extractedText.contains("-")) && x ){
+                    icBoundingBoxNum = i
+                }
+            }
+
+            Log.e("IC-FINAL-TAG", parsedText.textBlocks.get(icBoundingBoxNum).string)
+            // if len of string < 5, icBoundingBoxNum + 1
+
+
+            var nameBoundingBoxNum = 0;
+            nameBoundingBoxNum = icBoundingBoxNum +1
+            for (i in 1..parsedText.textBlocks.size) {
+                if (  parsedText.textBlocks.get(nameBoundingBoxNum).string.length < 7){
+                    nameBoundingBoxNum+=1
+                    }
+            }
+
+            Log.e("NAME-FINAL-TAG", parsedText.textBlocks.get(nameBoundingBoxNum).string)
+
+
 
             val file = File.createTempFile("MLKitTextOutput", ".json", App.context.cacheDir)
             val json = GsonBuilder().setPrettyPrinting().create().toJson(parsedText)
